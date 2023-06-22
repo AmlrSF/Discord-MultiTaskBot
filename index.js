665f7bec2463843949d75641ee7b20bff29fd8c8
Amir souaf <111384104+softyAmir@users.noreply.github.com>
2023-06-22 02:12:12 -1100

    Update index.js

--- index.js
+++ index.js
@@ -1,99 +1,55 @@
 const { 
 	Client,
-	Events,
 	Collection,
-	GatewayIntentBits,AttachmentBuilder,EmbedBuilder
+	GatewayIntentBits,
 } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-const { token,clientId, guildId } = require('./config.json');
-const Canvas = require('@napi-rs/canvas');
-const { log } = require('node:console');
-const { createCanvas, Image } = require('@napi-rs/canvas');
-const { readFile } = require('fs/promises');
-const { request } = require('undici');
+const { token } = require('./config.json');
+
 
 require('dotenv').config();
 
 
 const client = new Client({
-	 intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMembers,GatewayIntentBits.GuildMessages] 
-}).on('guildMemberAdd',(member)=>{
-	console.log(`${member.user.tag} has joined the server.`);
+	 intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages] 
 })
 
 client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-
-	// console.log(command);
-	
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
 	}
 }
 
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-	const command = interaction.client.commands.get(interaction.commandName);
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
-
-
-
-
-client.on(Events.GuildMemberAdd, async member => {
-	const welcomeChannelName = 'welcome'; // Replace with the name of your welcome channel
-	// log(member.user.username)
-	const welcomeChannel = member.guild.channels.cache.find(
-	  (channel) => channel.name === welcomeChannelName 
-	);
-	if (!welcomeChannel) return;
-	
-	// inside a command, event listener, etc.
-	const embed = new EmbedBuilder()
-	.setColor(0x0099FF)
-	.setTitle(`Welcome, ${member.user.username}! We hope you enjoy your stay in our server.`)
-	.setImage(member.user.displayAvatarURL())
-	.setTimestamp()
-
-
-
-	await welcomeChannel.send({
-		embeds : [embed],
-		ephemeral:true
-	})
-	
-});
-
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-
-});
+}
 
 
 
